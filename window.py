abb58aab93aebf35fd064e8a7619d84c311de90f
Orest Voloshchuk <orest.v@gmail.com>
2010-06-08 00:38:22 +0300

    FIX: розширив типи для клітинок

--- window.py
+++ window.py
@@ -5,12 +5,16 @@ import gtk.glade
 pygtk.require('2.0')
 import model;
 
-COLUMNS = {0: ('№', False), 1: ('Отримувач', False), 2: ('Тема', True), 
-           3: ('Відіслано', True), 4: ('Отримано', True), 
-           5: ('Квитанція', False)}
+COLUMNS = {0: ('№', False, gtk.CellRendererText, 'text'), 
+           1: ('Отримувач', False, gtk.CellRendererText, 'text'), 
+           2: ('Тема', True, gtk.CellRendererText, 'text'), 
+           3: ('Відіслано', True, gtk.CellRendererText, 'text'), 
+           4: ('Отримано', True, gtk.CellRendererText, 'text'), 
+           5: ('Квитанція', False, gtk.CellRendererToggle, 'active')}
 
 class Window:
     def __init__(self):
+        print '------------------------'
         self.gladefile = 'gui.glade'
         self.wTree = gtk.glade.XML(self.gladefile)
         self.window = self.wTree.get_widget('MainWindow')
@@ -21,13 +25,14 @@ class Window:
 
         self.tvLetters.set_model(model.TableModel())
         for i in COLUMNS.keys():
-            cell = gtk.CellRendererText()
+            cell = COLUMNS[i][2]()
             cell.set_data('column', i)
             if COLUMNS[i][1]:
                 cell.connect('edited', self.on_cell_edited, model)
                 cell.set_property('editable', True)
-            self.tvLetters.append_column(
-                gtk.TreeViewColumn(COLUMNS[i][0], cell, text=i))
+            column = gtk.TreeViewColumn(COLUMNS[i][0], cell)
+            column.add_attribute(cell, COLUMNS[i][3], i)
+            self.tvLetters.append_column(column)
 
         self.window.show_all()
 
