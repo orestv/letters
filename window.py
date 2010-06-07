ffbb43f418deac13948e8376c7f307e14c84dad6
Orest Voloshchuk <orest.v@gmail.com>
2010-06-08 00:15:19 +0300

    ADD: не всі колонки редагуються

--- window.py
+++ window.py
@@ -5,9 +5,9 @@ import gtk.glade
 pygtk.require('2.0')
 import model;
 
-COLUMNS = {0: '№', 1: 'Отримувач', 2: 'Тема', 
-           3: 'Відіслано', 4: 'Отримано', 
-           5: 'Квитанція'}
+COLUMNS = {0: ('№', False), 1: ('Отримувач', False), 2: ('Тема', True), 
+           3: ('Відіслано', True), 4: ('Отримано', True), 
+           5: ('Квитанція', False)}
 
 class Window:
     def __init__(self):
@@ -22,11 +22,12 @@ class Window:
         self.tvLetters.set_model(model.TableModel())
         for i in COLUMNS.keys():
             cell = gtk.CellRendererText()
-            cell.set_property('editable', True)
             cell.set_data('column', i)
-            cell.connect('edited', self.on_cell_edited, model)
+            if COLUMNS[i][1]:
+                cell.connect('edited', self.on_cell_edited, model)
+                cell.set_property('editable', True)
             self.tvLetters.append_column(
-                gtk.TreeViewColumn(COLUMNS[i], cell, text=i))
+                gtk.TreeViewColumn(COLUMNS[i][0], cell, text=i))
 
         self.window.show_all()
 
