d994ba893e25bf778f9770d5393e2f5053af4e59
Orest Voloshchuk <orest.v@gmail.com>
2010-06-12 16:34:20 +0300

    Змінив вивід організації

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class TableModel(gtk.GenericTreeModel):
     the tree are just the string representations of the nodes.'''
 
     COLUMNS = {0: {'name':'id', 'type':gobject.TYPE_INT}, 
-               1: {'name':'organization', 'type':gobject.TYPE_STRING},
+               1: {'name':'recipient_id', 'type':gobject.TYPE_INT},
                2: {'name':'subject', 'type':gobject.TYPE_STRING},
                3: {'name':'sent', 'type':gobject.TYPE_STRING},
                4: {'name':'received', 'type':gobject.TYPE_STRING},
@@ -33,13 +33,10 @@ class TableModel(gtk.GenericTreeModel):
         self.update_data()
         
     def update_data(self):
-        self.cn.query('''SELECT letters.id, organization, subject, 
-                      sent, received, receipt FROM letters 
-                      LEFT OUTER JOIN recipients
-                      ON letters.recipient_id = recipients.id''')
+        self.cn.query('''SELECT letters.id, recipient_id, subject, 
+                      sent, received, receipt FROM letters''')
         self.data = self.cn.store_result()
         self.data = self.data.fetch_row(maxrows=0, how=1)
-        print self.data
 
     def set_data(self, path, col, data):
         data = self.process_data(data, col)
@@ -107,3 +104,67 @@ class TableModel(gtk.GenericTreeModel):
         
     def on_iter_parent(self, node):
         return None
+
+class ComboModel(gtk.GenericTreeModel):
+    def __init__(self):
+        gtk.GenericTreeModel.__init__(self)
+        self.cn = MySQLdb.connect(read_default_file='~/.mysql.cnf',
+                                  db='letters')
+        self.update_data()
+
+    def update_data(self):
+        self.cn.query('''SELECT id, organization FROM recipients''')
+        self.data = self.cn.store_result()
+        self.data = self.data.fetch_row(maxrows=0)
+
+    def on_get_flags(self):
+        return gtk.TREE_MODEL_LIST_ONLY
+
+    def on_get_path(self, iter):
+        return str(iter[0]-1)
+
+    def on_get_n_columns(self):
+        return len(self.data[0])
+
+    def on_get_column_type(self, index):
+        return gobject.TYPE_STRING
+
+    def on_get_iter(self, path):
+        return path[0]
+
+    def on_get_value(self, node, column):
+        return self.data[node][column]
+
+    def on_iter_next(self, node):
+        result = None
+        if node != None:
+            if node < len(self.data)-1:
+                result = node+1
+        return result
+
+    def on_iter_children(self, iter):
+        return None
+
+    def on_iter_has_child(self, node):
+        return None
+
+    def on_iter_n_children(self, iter):
+        if not iter:
+            return len(self.data)
+        else:
+            return 0
+
+    def on_iter_nth_child(self, node, n):
+        if not node:
+            return self.data[n]
+        else:
+            return None
+        
+    def on_iter_parent(self, node):
+        return None
+
+
+
+
+
+
