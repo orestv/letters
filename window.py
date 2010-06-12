f5b654ad199243f7fe2e483325f16762035d277a
Orest Voloshchuk <orest.v@gmail.com>
2010-06-12 12:32:45 +0300

    Узагальнив сигнали і їх обробники для рендерерів клітинок - тепер передаються різні сигнали від усіх типів клітинок

--- window.py
+++ window.py
@@ -5,17 +5,17 @@ import gtk.glade
 pygtk.require('2.0')
 import model;
 
-COLUMNS = {0: {'name':'№', 'editable':False, 
+COLUMNS = {0: {'name':'№', 'editable':False,
                'renderer':gtk.CellRendererText, 'type':'text'}, 
            1: {'name':'Отримувач', 'editable':False,  
                'renderer':gtk.CellRendererText, 'type':'text'}, 
-           2: {'name':'Тема', 'editable':True,  
+           2: {'name':'Тема', 'editable':True,  'signal':'edited',
                'renderer':gtk.CellRendererText, 'type':'text'}, 
-           3: {'name':'Відіслано', 'editable':True,  
+           3: {'name':'Відіслано', 'editable':True,  'signal':'edited',
                'renderer':gtk.CellRendererText, 'type':'text'}, 
-           4: {'name':'Отримано', 'editable':True,  
+           4: {'name':'Отримано', 'editable':True, 'signal':'edited',
                'renderer':gtk.CellRendererText, 'type':'text'}, 
-           5: {'name':'Квитанція', 'editable':False,  
+           5: {'name':'Квитанція', 'editable':True,  'signal':'toggled',
                'renderer':gtk.CellRendererToggle, 'type':'active'}}
 
 class Window:
@@ -31,20 +31,31 @@ class Window:
 
         self.tvLetters.set_model(model.TableModel())
         for i in COLUMNS.keys():
-            cell = COLUMNS[i]['renderer']()
+            col = COLUMNS[i]
+            cell = col['renderer']()
             cell.set_data('column', i)
-            if COLUMNS[i]['editable']:
-                cell.connect('edited', self.on_cell_edited, model)
-                cell.set_property('editable', True)
+            if col['editable']:
+                if col['renderer'] == gtk.CellRendererToggle:
+                    cell.set_property('activatable', True)
+                    cell.connect('toggled', self.on_set_cell_toggle, model)
+                elif col['renderer'] == gtk.CellRendererText:
+                    cell.set_property('editable', True)
+                    cell.connect('edited', self.on_set_cell_text, model)
             column = gtk.TreeViewColumn(COLUMNS[i]['name'], cell)
             column.add_attribute(cell, COLUMNS[i]['type'], i)
             self.tvLetters.append_column(column)
 
         self.window.show_all()
 
-    def on_cell_edited(self, cell, path, new_text, model):
+    def on_set_cell_text(self, cell, path, new_text, model=None):
         column = cell.get_data('column')
-        self.tvLetters.get_model().set_data(path, column, new_text)
+        self.set_data(path, column, new_text)
+
+    def on_set_cell_toggle(self, cell, new_text, model):
+        pass
+
+    def set_data(self, path, column, data):
+        self.tvLetters.get_model().set_data(path, column, data)
 
 
 if __name__ == '__main__':
